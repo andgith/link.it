08b4ed1b6c3c13c9c8e58e706fc096d7f1cd3f09
Andrew Hirst <andrew.hirst@visavvi.com>
2025-02-02 20:02:54 +0000

    Encode link using API endpoint

--- app/Http/Controllers/API/LinkController.php
+++ app/Http/Controllers/API/LinkController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\EncodeLink;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DecodeLinkRequest;
+use App\Http\Requests\EncodeLinkRequest;
+use App\Http\Resources\LinkResource;
 
 class LinkController extends Controller
 {
-    public function encode(Request $request): JsonResponse
+    public function encode(EncodeLinkRequest $request, EncodeLink $action): LinkResource
     {
-        return response()->json();
+        $link = $action->handle($request->fluent());
+
+        return LinkResource::make($link);
     }
 
     public function decode(Request $request): JsonResponse
--- tests/Feature/Http/API/EncodeLinkTest.php
+++ tests/Feature/Http/API/EncodeLinkTest.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\Domain;
 use function Pest\Laravel\postJson;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Testing\Fluent\AssertableJson;
 
+beforeEach(function () {
+    $this->seed(DatabaseSeeder::class);
+});
 
 it('can encode a long url to short url', function () {
     $longUrl = 'https://www.thisisalongdomain.com/with/some/parameters?and=here_too';
@@ -11,9 +16,58 @@ it('can encode a long url to short url', function () {
         'url' => $longUrl
     ]);
 
-    $response->assertStatus(200);
+    $response->assertStatus(201);
 
     $response->assertJson(fn (AssertableJson $json) =>
-        $json->has('link')
+        $json->has('data.link')
+            ->has('data.url')
+            ->has('data.expires_at')
     );
+});
+
+it('generates link using default domain', function () {
+    $longUrl = 'https://www.thisisalongdomain.com/with/some/parameters?and=here_too';
+
+    $response = postJson('/encode', [
+        'url' => $longUrl
+    ]);
+
+    $response->assertStatus(201);
+
+    expect($response->json('data.link'))->toContain(Domain::default()->first()->name);
+});
+
+it('generates link using specified domain', function () {
+    $longUrl = 'https://www.thisisalongdomain.com/with/some/parameters?and=here_too';
+
+    // Additional domain
+    $domain = Domain::factory()->create();
+
+    $response = postJson('/encode', [
+        'url' => $longUrl,
+        'domain' => $domain->name
+    ]);
+
+    $response->assertStatus(201);
+
+    expect($response->json('data.link'))->toContain($domain->name);
+});
+
+it('it validates domain', function () {
+    $longUrl = 'https://www.thisisalongdomain.com/with/some/parameters?and=here_too';
+
+    $response = postJson('/encode', [
+        'url' => $longUrl,
+        'domain' => 'nonexistent.com'
+    ]);
+
+    $response->assertStatus(422);
+});
+
+it('it validates url', function () {
+    $response = postJson('/encode', [
+        'url' => 'invalid-url'
+    ]);
+
+    $response->assertStatus(422);
 });
